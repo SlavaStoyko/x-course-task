e053a39a6b0bf263437b4b260f6db8e0ac1f02bd
Vyacheslav <myworknm@ukr.net>
2024-03-23 14:33:44 +0200

    portal + btnSignIn validation1

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,26 +1,28 @@
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { useNavigate } from 'react-router-dom';
 import classnames from "classnames";
 import {LS_KEYS, LocalStorageService } from '../localStorage/localStorage';
 import './stylesignin.css';
 import avatar from '../../images/avatar.png';
-import sprite from "../../images/sprites.svg";
 import { SignInPortalValidation } from './SignInPortalValidation';
 
 export const SignIn = ({islogin}) => {
+
     const [isswitchOn, setIsSwitchOn] = useState(true);
     const navigate = useNavigate();
     const [inputvalue,setinputvalue] = useState('');
+    useEffect(()=>{
+        if(inputvalue.length >=4 && inputvalue.length <=16){
+            setIsSwitchOn(false);
+        }else{
+            setIsSwitchOn(true);
     
+        }
+    },[inputvalue])
 const getInput = ({target:{value}})=>{
     setinputvalue(value.trim());
-    if(inputvalue.length >=4 && inputvalue.length <=16){
-        setIsSwitchOn(false);
-    }else{
-        setIsSwitchOn(true);
-
-    }
+    
 }
 const handlelLSLogin = () =>{
     if(inputvalue.length >=4 && inputvalue.length <=16){
@@ -32,25 +34,12 @@ const handlelLSLogin = () =>{
   
    
 }
-//inputRef.current.focus();
-//console.log(inputRef);
    return (
     <div className="conteiner">
         <div className="flexform ">
             <div className="imgpicture">
                 <div className="wraplogoimg">
                         <img className="pictureuser" src={avatar} alt="logo pictures"></img>
-                    {/* <div className="identify">
-                        { inputvalue.length >=4 && inputvalue.length <=16 ?
-                        <svg className="identifyItem">
-                            <use xlinkHref={`${sprite}#done`}></use>
-                        </svg>
-                        :
-                        <svg className={ inputvalue.length === 0 ? "identifyItem higenstatus": "identifyItem"}>
-                            <use xlinkHref={`${sprite}#close`}></use>
-                        </svg> 
-                            }                   
-                    </div> */}
                 </div>
             </div>
 
